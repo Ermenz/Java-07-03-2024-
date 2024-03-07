9b9a6a842b99191a7e86d992dd00cb60320b89bb
Ermenz <143419782+Ermenz@users.noreply.github.com>
2024-03-07 10:12:55 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   float media;
+	   
+	  float N1 = 6;
+	  float N2 = 7;
+	  float N3 = 7;
+	  float N4 = 9;
+	   
+	   
+      media = (N1 + N2 + N3 + N4)/4;
+	
+	  System.out.println(media);   
+	 
+	 if(media >= 7) {
+	  System.out.println("Aluno aprovado");   
+	 } else {
+	  System.out.println("Aluno reprovado");   
+	     
+	 }
+	  	
+	  
+	}
+}
